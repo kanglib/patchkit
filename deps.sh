4c98df9206281891821e0f523e5e56b5f625bbed
Hyeonwoo Kang <kanglib14@gmail.com>
2018-08-10 17:22:55 +0900

    Update deps.sh

--- deps.sh
+++ deps.sh
@@ -1,39 +1,22 @@
-#!/bin/bash -u
+#!/bin/bash -eu
 
-echo
-echo "Take a look here if Unicorn fails to build:"
-echo " https://github.com/unicorn-engine/unicorn/blob/master/docs/COMPILE-NIX.md"
-echo
-echo "If you're on Ubuntu, you want to do this first:"
-echo " sudo apt-get update"
-echo " sudo apt-get install python-pip build-essential git cmake python-dev libglib2.0-dev"
-echo
-echo "If you're on a Mac, do this first:"
-echo " brew install pkg-config glib cmake"
-echo
-echo "Using ./build as a tmp dir. ^C if that's a bad idea."
-echo
-echo -n "[press enter to continue]"
-read
-echo
+sudo apt-get update
+sudo apt-get install python-pip build-essential git cmake python-dev libglib2.0-dev -y
 
-cwd=$(pwd)
-build="$cwd/build"
-
-mkdir build &>/dev/null
-set -e
+build=~+/build
+mkdir -p build
 
 echo "[*] Building Keystone"
 cd "$build"
 git clone https://github.com/keystone-engine/keystone.git
 cd keystone && mkdir build && cd build
-cmake -DCMAKE_BUILD_TYPE=Release -DBUILD_SHARED_LIBS=ON -DLLVM_TARGETS_TO_BUILD="all" -G "Unix Makefiles" .. && make -j2
+cmake -DCMAKE_BUILD_TYPE=Release -DBUILD_SHARED_LIBS=ON -DLLVM_TARGETS_TO_BUILD="X86;ARM" -G "Unix Makefiles" .. && make -j$(nproc)
 echo
 
 echo "[*] Building Capstone"
 cd "$build"
 git clone https://github.com/aquynh/capstone.git
-cd capstone && make -j2
+cd capstone && make -j$(nproc)
 echo
 
 echo "[*] Building Unicorn"
@@ -42,7 +25,7 @@ git clone https://github.com/unicorn-engine/unicorn.git
 cd unicorn && ./make.sh
 
 echo
-echo "[*] Installing projects and Python bindings (using sudo)"
+echo "[*] Installing projects and Python bindings"
 cd "$build/keystone/build" && sudo make install
 cd "$build/keystone/bindings/python" && sudo make install
 
@@ -54,8 +37,6 @@ cd "$build/unicorn/bindings/python" && sudo make install
 
 which ldconfig &>/dev/null && sudo ldconfig
 
-echo
-echo "All done!"
 echo
 echo -n "Testing Python import: "
 python -c "import capstone, keystone, unicorn; capstone.CS_ARCH_X86, unicorn.UC_ARCH_X86, keystone.KS_ARCH_X86; print 'works.'"
